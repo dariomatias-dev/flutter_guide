240b84ff11b3c3972bd5ca822cc6fb8a420d5749
dariomatias-dev <matiasdario75@gmail.com>
2024-04-03 08:13:20 -0300

    feat(lib/src/features/saved_widgets): added display of saved widgets to the saved widgets screen
    - Display of saved widgets added to the saved widgets screen.

--- lib/src/features/saved_widgets/saved_widgets_screen.dart
+++ lib/src/features/saved_widgets/saved_widgets_screen.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_guide/src/core/constants/widgets.dart';
+
 import 'package:flutter_guide/src/providers/user_preferences_inherited_widget.dart';
 
+import 'package:flutter_guide/src/shared/models/widget_model.dart';
 import 'package:flutter_guide/src/shared/widgets/back_button_widget.dart';
 import 'package:flutter_guide/src/shared/widgets/change_theme_button_widget/change_theme_button_widget.dart';
+import 'package:flutter_guide/src/shared/widgets/widget_list_widget.dart';
 
-class SavedWidgets extends StatelessWidget {
+class SavedWidgets extends StatefulWidget {
   const SavedWidgets({super.key});
 
-  void getSavedWidgets(BuildContext context) {
+  @override
+  State<SavedWidgets> createState() => _SavedWidgetsState();
+}
+
+class _SavedWidgetsState extends State<SavedWidgets> {
+  late List<WidgetModel> flutterWidgets = [];
+
+  void getSavedWidgets() {
     final sharedPreferences =
         UserPreferencesInheritedWidget.of(context)!.sharedPreferences;
-    final savedWidgets = sharedPreferences.getStringList('saved_widgets');
-    print(savedWidgets);
+    final savedWidgets = sharedPreferences.getStringList('saved_widgets')!;
+
+    final items = <WidgetModel>[];
+
+    for (WidgetModel widgetItem in widgets) {
+      if (savedWidgets.contains(widgetItem.name)) {
+        items.add(widgetItem);
+      }
+    }
+
+    flutterWidgets = items;
+  }
+
+  @override
+  void didChangeDependencies() {
+    getSavedWidgets();
+
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-    getSavedWidgets(context);
-
     return Scaffold(
       appBar: AppBar(
         leading: const BackButtonWidget(),
@@ -33,7 +58,17 @@ class SavedWidgets extends StatelessWidget {
           ChangeThemeButtonWidget(),
         ],
       ),
-      body: Container(),
+      body: flutterWidgets.isNotEmpty
+          ? SingleChildScrollView(
+              child: WidgetListWidget(
+                widgets: flutterWidgets,
+              ),
+            )
+          : const Center(
+              child: Text(
+                'No widgets have been saved yet.',
+              ),
+            ),
     );
   }
 }
