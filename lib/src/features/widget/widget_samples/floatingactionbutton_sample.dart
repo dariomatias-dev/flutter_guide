ed46f15026fa2283ad65303a6f3d3a6cb8d71732
dariomatias-dev <matiasdario75@gmail.com>
2024-04-16 19:05:13 -0300

    feat(lib/src/features/widget/widget_samples/floatingactionbutton_sample.dart): improved floating action button example
    - Improved example of floating action button.

--- lib/src/features/widget/widget_samples/floatingactionbutton_sample.dart
+++ lib/src/features/widget/widget_samples/floatingactionbutton_sample.dart
@@ -6,11 +6,55 @@ class FloatingActionButtonSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const SingleChildScrollView(
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 200.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const FloatingActionButtonDefault();
+                    },
+                  ),
+                );
+              },
+              child: const Text('Default'),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class FloatingActionButtonDefault extends StatelessWidget {
+  const FloatingActionButtonDefault({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        surfaceTintColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new_rounded,
+            size: 20.0,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: List.generate(20, (index) {
+            return ListTile(
+              title: Text('Title ${index + 1}'),
+            );
+          }),
         ),
       ),
       floatingActionButton: FloatingActionButton(
