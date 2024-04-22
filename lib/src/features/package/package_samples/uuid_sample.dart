66fd7b94f767cb505fb1ca2e166d9eb98eb176a2
dariomatias-dev <matiasdario75@gmail.com>
2024-04-22 18:38:53 -0300

    feat(lib/src/features/package/package_samples/uuid_sample.dart): enhanced uuid package example
    - Added generation of UUIDs for versions 1, 4, 5, 6, 7, and 8.

--- lib/src/features/package/package_samples/uuid_sample.dart
+++ lib/src/features/package/package_samples/uuid_sample.dart
@@ -1,6 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
+class UuidVersionModel {
+  const UuidVersionModel({
+    required this.version,
+    required this.generator,
+  });
+
+  final int version;
+  final String Function() generator;
+}
+
+const uuid = Uuid();
+
+final uuidVersions = <UuidVersionModel>[
+  UuidVersionModel(
+    version: 1,
+    generator: () {
+      return uuid.v1();
+    },
+  ),
+  UuidVersionModel(
+    version: 4,
+    generator: () {
+      return uuid.v4();
+    },
+  ),
+  UuidVersionModel(
+    version: 5,
+    generator: () {
+      return uuid.v5(
+        Uuid.NAMESPACE_URL,
+        'https://flutter.dev',
+      );
+    },
+  ),
+  UuidVersionModel(
+    version: 6,
+    generator: () {
+      return uuid.v6();
+    },
+  ),
+  UuidVersionModel(
+    version: 7,
+    generator: () {
+      return uuid.v7();
+    },
+  ),
+  UuidVersionModel(
+    version: 8,
+    generator: () {
+      return uuid.v8();
+    },
+  ),
+];
+
 class UuidSample extends StatefulWidget {
   const UuidSample({super.key});
 
@@ -9,13 +63,13 @@ class UuidSample extends StatefulWidget {
 }
 
 class _UuidSampleState extends State<UuidSample> {
+  int _uuidVersionIndex = 0;
+
   String _uuid = '';
 
   void _generateUuid() {
-    const uuid = Uuid();
-
     setState(() {
-      _uuid = uuid.v4();
+      _uuid = uuidVersions[_uuidVersionIndex].generator();
     });
   }
 
@@ -34,6 +88,25 @@ class _UuidSampleState extends State<UuidSample> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(_uuid),
+            const SizedBox(height: 12.0),
+            DropdownButtonHideUnderline(
+              child: DropdownButton(
+                value: _uuidVersionIndex,
+                items: List.generate(uuidVersions.length, (index) {
+                  final uuidVersion = uuidVersions[index];
+
+                  return DropdownMenuItem(
+                    value: index,
+                    child: Text('Version ${uuidVersion.version}'),
+                  );
+                }),
+                onChanged: (value) {
+                  setState(() {
+                    _uuidVersionIndex = value!;
+                  });
+                },
+              ),
+            ),
             const SizedBox(height: 12.0),
             ElevatedButton(
               onPressed: _generateUuid,
