fdd51a5c795dc3d8a6bd971ee178a97f9d3cff6b
dariomatias-dev <matiasdario75@gmail.com>
2024-04-26 19:50:29 -0300

    feat(lib/src/features/widget/widget_samples/rangeslider_sample.dart): enhanced RangeSlider widget example
    - Improved RangeSlider widget example with additional use cases.

--- lib/src/features/widget/widget_samples/rangeslider_sample.dart
+++ lib/src/features/widget/widget_samples/rangeslider_sample.dart
@@ -1,37 +1,120 @@
 import 'package:flutter/material.dart';
 
-class RangeSliderSample extends StatefulWidget {
+class RangeSliderSample extends StatelessWidget {
   const RangeSliderSample({super.key});
 
   @override
-  State<RangeSliderSample> createState() => _RangeSliderSampleState();
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 12.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RangeSliderTemplate(
+                title: 'Standard',
+              ),
+              Divider(),
+              RangeSliderTemplate(
+                title: '0 to 100 without divisions',
+                max: 100.0,
+              ),
+              Divider(),
+              RangeSliderTemplate(
+                title: '50 to 100 with 50 divisions',
+                min: 50.0,
+                max: 100.0,
+                divisions: 50,
+              ),
+              Divider(),
+              RangeSliderTemplate(
+                title: '0 to 100 with 20 divisions',
+                max: 100.0,
+                divisions: 20,
+              ),
+              Divider(),
+              RangeSliderTemplate(
+                title: '0 to 100 with 100 divisions',
+                max: 100.0,
+                divisions: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _RangeSliderSampleState extends State<RangeSliderSample> {
+class RangeSliderTemplate extends StatefulWidget {
+  const RangeSliderTemplate({
+    super.key,
+    required this.title,
+    this.min = 0.0,
+    this.max = 1.0,
+    this.divisions,
+  });
+
+  final String title;
+  final double min;
+  final double max;
+  final int? divisions;
+
+  @override
+  State<RangeSliderTemplate> createState() => _RangeSliderTemplateState();
+}
+
+class _RangeSliderTemplateState extends State<RangeSliderTemplate> {
+  bool _showFloatingPoint = true;
   RangeValues _values = const RangeValues(
     0.0,
     1.0,
   );
 
   @override
+  void initState() {
+    _values = RangeValues(
+      widget.min,
+      widget.max,
+    );
+
+    if (widget.divisions != null) {
+      final divisionResult = widget.max / widget.divisions!;
+      _showFloatingPoint = divisionResult != divisionResult.floor();
+    }
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('${_values.start} - ${_values.end}'),
-            RangeSlider(
-              values: _values,
-              onChanged: (value) {
-                setState(() {
-                  _values = value;
-                });
-              },
-            ),
-          ],
+    return Column(
+      children: <Widget>[
+        Text(
+          widget.title,
+          style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
         ),
-      ),
+        Text(
+          '${_showFloatingPoint ? _values.start : _values.start.floor()} - ${_showFloatingPoint ? _values.end : _values.end.floor()}',
+        ),
+        RangeSlider(
+          values: _values,
+          min: widget.min,
+          max: widget.max,
+          divisions: widget.divisions,
+          onChanged: (value) {
+            setState(() {
+              _values = value;
+            });
+          },
+        ),
+      ],
     );
   }
 }
