a91d4f15e2a987ad30ecf670e65ec548d6aa354a
dariomatias-dev <matiasdario75@gmail.com>
2024-05-01 18:53:04 -0300

    feat(lib/src/features/examples): created custom popup menu example
    - Custom popup menu example created.

---
+++ lib/src/features/examples/samples/custom_popup_menu_sample.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+class AlignmentModel {
+  const AlignmentModel({
+    required this.name,
+    required this.value,
+  });
+
+  final String name;
+  final Alignment value;
+}
+
+const alignments = <AlignmentModel>[
+  AlignmentModel(
+    name: 'Bottom Center',
+    value: Alignment.bottomCenter,
+  ),
+  AlignmentModel(
+    name: 'Bottom Left',
+    value: Alignment.bottomLeft,
+  ),
+  AlignmentModel(
+    name: 'Bottom Right',
+    value: Alignment.bottomRight,
+  ),
+  AlignmentModel(
+    name: 'Center',
+    value: Alignment.center,
+  ),
+  AlignmentModel(
+    name: 'Center Left',
+    value: Alignment.centerLeft,
+  ),
+  AlignmentModel(
+    name: 'Center Right',
+    value: Alignment.centerRight,
+  ),
+  AlignmentModel(
+    name: 'Top Center',
+    value: Alignment.topCenter,
+  ),
+  AlignmentModel(
+    name: 'Top Left',
+    value: Alignment.topLeft,
+  ),
+  AlignmentModel(
+    name: 'Top Right',
+    value: Alignment.topRight,
+  ),
+];
+
+class CustomPopupMenuSample extends StatefulWidget {
+  const CustomPopupMenuSample({super.key});
+
+  @override
+  State<CustomPopupMenuSample> createState() => CcustoPpopuMmenSsampleState();
+}
+
+class CcustoPpopuMmenSsampleState extends State<CustomPopupMenuSample> {
+  final _buttonKey = GlobalKey();
+
+  OverlayEntry? _overlayEntry;
+  Alignment _buttonAlignment = Alignment.center;
+
+  void _showMenu() {
+    final rendexBox =
+        _buttonKey.currentContext!.findRenderObject() as RenderBox;
+    final buttonPosition = rendexBox.localToGlobal(Offset.zero);
+    final screenSize = MediaQuery.sizeOf(context);
+    final screenWidth = screenSize.width;
+    final screenHeight = screenSize.height;
+    final buttonSize = rendexBox.size;
+    final isButtonAtHorizontalScreenEdge =
+        buttonPosition.dx + buttonSize.width == screenWidth;
+    final isButtonAtVerticalScreenEdge =
+        buttonPosition.dy + buttonSize.height + 120 >= screenHeight;
+
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Stack(
+        children: [
+          GestureDetector(
+            onTap: _removeOverlay,
+            child: Container(
+              color: Colors.transparent,
+              constraints: const BoxConstraints.expand(),
+            ),
+          ),
+          Positioned(
+            top: isButtonAtVerticalScreenEdge
+                ? null
+                : buttonPosition.dy + buttonSize.height + 4.0,
+            right: isButtonAtHorizontalScreenEdge ? 0.0 : null,
+            bottom: isButtonAtVerticalScreenEdge
+                ? screenHeight - buttonPosition.dy + 4.0
+                : null,
+            left: isButtonAtHorizontalScreenEdge ? null : buttonPosition.dx,
+            child: Material(
+              color: Colors.transparent,
+              child: Container(
+                width: 120.0,
+                height: 120.0,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: const Center(
+                  child: Text('Menu'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _removeOverlay() {
+    _overlayEntry!.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Align(
+              alignment: _buttonAlignment,
+              child: ElevatedButton(
+                key: _buttonKey,
+                onPressed: _showMenu,
+                child: const Text('Open'),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 12.0,
+            ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                value: _buttonAlignment,
+                items: List.generate(alignments.length, (index) {
+                  final alignment = alignments[index];
+
+                  return DropdownMenuItem(
+                    value: alignment.value,
+                    child: Text(alignment.name),
+                  );
+                }),
+                onChanged: (value) {
+                  setState(() {
+                    _buttonAlignment = value!;
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
