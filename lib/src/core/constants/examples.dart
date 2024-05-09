5c0824b4a8e2a5c1ea7a5742eb41db560db6f19c
dariomatias-dev <matiasdario75@gmail.com>
2024-05-09 06:49:59 -0300

    feat(lib/src): added gap example
    - Added a gap example, providing code for implementing a spacing feature between elements.

--- lib/src/core/constants/examples.dart
+++ lib/src/core/constants/examples.dart
@@ -1,5 +1,6 @@
 // Examples
 import 'package:flutter_guide/src/features/example/samples/custom_popup_menu_sample.dart';
+import 'package:flutter_guide/src/features/example/samples/gaps_sample.dart';
 
 import 'package:flutter_guide/src/shared/models/example_model.dart';
 
@@ -7,5 +8,9 @@ const examples = <ExampleModel>[
   ExampleModel(
     name: 'Custom Popup Menu',
     component: CustomPopupMenuSample(),
+  ),
+  ExampleModel(
+    name: 'Gaps',
+    component: GapsSample(),
   ),
 ];
---
+++ lib/src/features/example/samples/gaps_sample.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+const programmingLanguages = <String>[
+  'Dart',
+  'Python',
+  'JavaScript',
+  'Java',
+  'C#',
+  'C++',
+  'C',
+  'GoLang',
+  'Kotlin',
+  'Swift',
+];
+
+class GapsSample extends StatefulWidget {
+  const GapsSample({super.key});
+
+  @override
+  State<GapsSample> createState() => _GapsSampleState();
+}
+
+class _GapsSampleState extends State<GapsSample> {
+  double _gap = 12.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: List.generate(
+              programmingLanguages.length * 2 - 1,
+              (index) {
+                if (index % 2 == 1) {
+                  return SizedBox(height: _gap);
+                }
+
+                return Text(
+                  programmingLanguages[index ~/ 2],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.blue.shade50.withOpacity(0.6),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const SizedBox(height: 6.0),
+            Text(
+              'Gap: $_gap',
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Slider(
+              value: _gap,
+              min: 0.0,
+              max: 40.0,
+              divisions: 20,
+              onChanged: (value) {
+                setState(() {
+                  _gap = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
