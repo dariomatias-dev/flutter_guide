d40b1384aa6a19a49d3fb35c7424b09061e66b37
dariomatias-dev <matiasdario75@gmail.com>
2024-05-13 12:37:13 -0300

    feat(lib/src/shared/widgets/custom_dialog): componentized custom dialog screen
    - Custom dialog screen componentized aiming for code organization.

--- lib/src/shared/widgets/custom_dialog/custom_dialog.dart
+++ lib/src/shared/widgets/custom_dialog/custom_dialog.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 part 'action_button_widget.dart';
 part 'dialog_widget.dart';
 part 'line_widget.dart';
+part 'custom_dialog_screen_widget.dart';
 
 class CustomDialog {
   OverlayEntry? _overlayEntry;
@@ -45,53 +46,11 @@ class CustomDialog {
   }) {
     _overlayEntry = OverlayEntry(
       builder: (context) {
-        return Stack(
-          children: <Widget>[
-            GestureDetector(
-              onTap: _removeDocs,
-              child: Container(
-                color: Colors.black.withOpacity(0.6),
-                constraints: const BoxConstraints.expand(),
-                child: Container(
-                  color: Colors.blue.shade100.withOpacity(
-                    Theme.of(context).brightness == Brightness.light
-                        ? 0.3
-                        : 0.0,
-                  ),
-                  constraints: const BoxConstraints.expand(),
-                ),
-              ),
-            ),
-            Center(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const IgnorePointer(
-                    child: SizedBox(width: 32.0),
-                  ),
-                  Expanded(
-                    child: Material(
-                      color: Colors.transparent,
-                      child: Container(
-                        padding: const EdgeInsets.all(12.0),
-                        width: MediaQuery.sizeOf(context).width,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.background,
-                          borderRadius: BorderRadius.circular(32.0),
-                        ),
-                        child: builder(
-                          _overlayEntry,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const IgnorePointer(
-                    child: SizedBox(width: 32.0),
-                  ),
-                ],
-              ),
-            ),
-          ],
+        return CustomDialogScreenWidget(
+          removeDocs: _removeDocs,
+          child: builder(
+            _overlayEntry,
+          ),
         );
       },
     );
---
+++ lib/src/shared/widgets/custom_dialog/custom_dialog_screen_widget.dart
@@ -0,0 +1,60 @@
+part of 'custom_dialog.dart';
+
+class CustomDialogScreenWidget extends StatelessWidget {
+  const CustomDialogScreenWidget({
+    super.key,
+    required this.removeDocs,
+    required this.child,
+  });
+
+  final VoidCallback removeDocs;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        GestureDetector(
+          onTap: removeDocs,
+          child: Container(
+            color: Colors.black.withOpacity(0.6),
+            constraints: const BoxConstraints.expand(),
+            child: Container(
+              color: Colors.blue.shade100.withOpacity(
+                Theme.of(context).brightness == Brightness.light ? 0.3 : 0.0,
+              ),
+              constraints: const BoxConstraints.expand(),
+            ),
+          ),
+        ),
+        Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const IgnorePointer(
+                child: SizedBox(width: 32.0),
+              ),
+              Expanded(
+                child: Material(
+                  color: Colors.transparent,
+                  child: Container(
+                    padding: const EdgeInsets.all(12.0),
+                    width: MediaQuery.sizeOf(context).width,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.background,
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                    child: child,
+                  ),
+                ),
+              ),
+              const IgnorePointer(
+                child: SizedBox(width: 32.0),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
