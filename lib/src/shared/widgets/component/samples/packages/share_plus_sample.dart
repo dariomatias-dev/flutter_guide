0eca19a0ddbdaff497ede9afb30831692edca49e
dariomatias-dev <matiasdario75@gmail.com>
2024-08-10 19:59:47 -0300

    feat(lib/src/shared/widgets/component/samples/packages/share_plus_sample.dart): improved package sample
    - Improved the package sample by adding result handling and possible error management.

--- lib/src/shared/widgets/component/samples/packages/share_plus_sample.dart
+++ lib/src/shared/widgets/component/samples/packages/share_plus_sample.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:share_plus/share_plus.dart';
 
-class SharePlusSample extends StatelessWidget {
+class SharePlusSample extends StatefulWidget {
   const SharePlusSample({super.key});
+
+  @override
+  State<SharePlusSample> createState() => _SharePlusSampleState();
+}
+
+class _SharePlusSampleState extends State<SharePlusSample> {
+  final _logger = Logger();
+
+  Future<void> _shareText() async {
+    try {
+      final result = await Share.share(
+        'https://flutter.dev',
+      );
+
+      if (result.status == ShareResultStatus.dismissed ||
+          result.status == ShareResultStatus.unavailable) {
+        _logger.e(
+          'Warning Log',
+          error: result.raw,
+        );
+      }
+    } catch (err, stackTrace) {
+      _logger.e(
+        'Error Log',
+        error: err,
+        stackTrace: stackTrace,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            Share.share(
-              'https://flutter.dev',
-            );
-          },
-          child: const Text('Share URL'),
+          onPressed: _shareText,
+          child: const Text(
+            'Share URL',
+          ),
         ),
       ),
     );
